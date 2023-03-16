4769c67fd54de6a502e847324469aab328252a57
Sayam1007 <sayamsobti@gmail.com>
2023-03-16 19:48:08 +0530

    first commit

---
+++ script.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+# shell script to install and configure git
+
+if ! [ -x "$(command -v git)" ];then
+echo "git not found on your system"
+echo "installing git"
+sudo apt-get update -y
+sudo apt-get install git -y
+echo "Git installed"
+git --version
+
+echo "Configuring git "
+echo "Enter your Github email id"
+read mail
+echo "Enter your Github Username"
+read username
+
+git config --global user.mail "$mail"
+git config --global user.name "$username"
+echo "Configured"
+
+else
+echo "git found on your system"
+git --version
+fi
